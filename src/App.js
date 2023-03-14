7ac7acc389d2a0155aa2363c0902ef1dcce4d6a6
gowtham0927 <gowthamsaiyadlapalli1234@gmail.com>
2023-03-14 14:30:53 +0530

    Routings done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import Navbar from './Components/Home';
-import { BrowserRouter as Router, Routes
-,Route } from 'react-router-dom';
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homes from "./Components/Homes";
+import About from "./Components/About";
+import Browse from "./Components/Browse";
+import Contact from "./Components/Contact";
+import Create from "./Components/Create";
 
-import Home from './Components/Home';
 function App() {
   return (
-    <div >
-     {/* <Navbar /> */}
-     <Router>
-      <Routes>
-        <Route exact path='/' element={<Home />}/>
-      </Routes>
-     </Router>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Homes />}></Route>
+          <Route exact path="/About" element={<About />}></Route>
+          <Route exact path="/browse" element={<Browse />}></Route>
+          <Route exact path="/contact us" element={<Contact />}></Route>
+          <Route exact path="/create" element={<Create />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <p>This is to contact us....</p>
+    </div>
+  )
+}
